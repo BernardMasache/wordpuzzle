17371ead24e89de4325a2a8667a66aae9ef98364
Bernard Masache <esc-64-15@cc.ac.mw>
2019-05-08 19:00:20 -0700

    ui update

--- src/components/navigationbar.js
+++ src/components/navigationbar.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import {Navbar, Nav, Form, FormControl, Button, CardGroup} from 'react-bootstrap';
+import {Navbar, Nav, Form, Button, CardGroup} from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
 import Mouse from '../images/Mouse-icon-for-web.png';
 import Mouse2 from '../images/geometry.jpg';
 import Mouse3 from '../images/grammer.png';
 import Mouse4 from '../images/Arithmetic_symbols.svg.png';
 
 class Navigationbar extends React.Component{
-    constructor(){
-        super();
+    constructor(props, context){
+        super(props, context);
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
         this.state={
             title: 'Word Puzzle',
-            isDisplayed: true
+            isDisplayed: true,
+            show: false,
         }
+
+        
+    }
+    handleClose() {
+      this.setState({ show: false });
+    }
+  
+    handleShow() {
+      this.setState({ show: true });
     }
 
     handleDisplay(e){
@@ -44,8 +57,6 @@ class Navigationbar extends React.Component{
                 </Nav>
                 <Form inline>
                 <Nav.Link to="#register">Register</Nav.Link>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" onChange={this.handleChange} />
-                <Button variant="outline-success">Search</Button>
                 </Form>
             </Navbar.Collapse>
             </Navbar>
@@ -62,15 +73,36 @@ class Navigationbar extends React.Component{
                 <Card.Text>
                   Enjoy the school exercise <span style={{color: '#d58c0d'}}>GAME</span>
                 </Card.Text>
+
+                <Button variant="primary" onClick={this.handleShow}  style={{color: '#d58c0d', background: 'transparent'}}> Signin to start the GAME</Button>
+
+                <Modal show={this.state.show} onHide={this.handleClose} style={{textAlign: "center", marginTop:'6.3%', color: '#d58c0d'}}>
                 
-                <Button variant="primary" style={{color: '#d58c0d', background: 'transparent'}}>Signin to start the GAME</Button>
+                
+                <Form>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label style={{ fontSize: '5rem', fontFamily: 'fantasy'}}>SIGNIN</Form.Label>
+                    <div style={{marginLeft:'15%', marginRight: '15%'}}>
+                    <Form.Control type="email" placeholder="Enter email" required/><br/>
+                    <Form.Control type="password" placeholder="Enter password" required/>
+                    </div><br/>
+                    <Modal.Footer>
+                  <Button variant="primary" onClick={this.handleClose} style={{background:'transparent', color: '#d58c0d', fontFamily: 'fantasy'}}> Submit </Button>
+                  <Button variant="secondary" onClick={this.handleClose} style={{marginRight:'30%', background:'dark', color: '#fff', fontFamily: 'fantasy'}}> Cancel</Button>
+                </Modal.Footer>
+                  </Form.Group>
+                  </Form>
+          
+                
+              </Modal>
+                
             </Card.ImgOverlay>
             </Card>
 
          </div>
          <div>
          <Card className="text-center">
-  <Card.Header>GRAMMAR | ARITHMETIC | GEOMETRY</Card.Header>
+  <Card.Header>className={`text-muted ${isDisplayed ? 'is-displayed' : ''}`}GRAMMAR | ARITHMETIC | GEOMETRY</Card.Header>
   <Card.Body>
     <Card.Title>Special title treatment</Card.Title>
     <Card.Text>
@@ -79,9 +111,8 @@ class Navigationbar extends React.Component{
     <Button variant="primary" onClick={(e) => this.handleDisplay(e)}>Tutorials</Button>
   </Card.Body> 
   <Card.Footer className="text-muted">
-
-  <CardGroup className={`text-muted ${isDisplayed ? 'is-displayed' : ''}`}>
-  <Card >
+  <CardGroup>
+  <Card className="card-item">
     <Card.Img variant="top" src={Mouse4}  style={{height:200}}/>
     <Card.Body>
       <Card.Title>Arithmetic</Card.Title>
@@ -94,7 +125,7 @@ class Navigationbar extends React.Component{
       <small className="text-muted">Last updated 3 mins ago</small>
     </Card.Footer>
   </Card>
-  <Card>
+  <Card className="card-item">
     <Card.Img variant="top" src={Mouse3} style={{height:200}}/>
     <Card.Body>
       <Card.Title>Grammer</Card.Title>
@@ -107,7 +138,7 @@ class Navigationbar extends React.Component{
       <small className="text-muted">Last updated 3 mins ago</small>
     </Card.Footer>
   </Card>
-  <Card>
+  <Card className="card-item">
     <Card.Img variant="top" src={Mouse2} style={{height:200}}/>
     <Card.Body>
       <Card.Title>Geometry</Card.Title>
